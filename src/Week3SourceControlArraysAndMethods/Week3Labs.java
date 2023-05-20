8c5e4ae7cdb57e98d06d178d3be719b5bb478381
Mario Morales <132522679+morales99889@users.noreply.github.com>
2023-05-20 16:47:43 -0400

    week3 labs commit

---
+++ src/Week3SourceControlArraysAndMethods/Week3Labs.java
@@ -0,0 +1,205 @@
+package Week3SourceControlArraysAndMethods;
+
+public class Week3Labs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				//
+				// Arrays:
+				//
+				
+				// 1. Create an array with the following values 1, 5, 2, 8, 13, 6
+				int[] array = {1, 5, 2, 8, 13, 6};
+				
+				
+				// 2. Print out the first element in the array
+				System.out.println(array[0]);
+				
+				// 3. Print out the last element in the array without using the number 5
+				System.out.println(array[array.length - 1]);
+				
+				// 4. Print out the element in the array at position 6, what happens?
+				//System.out.println(array[6]);
+				
+				// 5. Print out the element in the array at position -1, what happens?
+				//System.out.println(array[-1]);
+					
+				// 6. Write a traditional for loop that prints out each element in the array
+				for (int i=0; i < 6; i++) {
+					System.out.println(array[i]);
+				}
+					
+				// 7. Write an enhanced for loop that prints out each element in the array
+				for (int element: array) {
+					System.out.println(element);
+				}
+				
+				// 8. Create a new variable called sum, write a loop that adds 
+				//			each element in the array to the sum
+				int sum = 0;
+				for (int element : array) {
+					sum += element; 
+				}
+					
+				// 9. Create a new variable called average and assign the average value of the array to it
+				int average = sum / array.length;
+				
+				System.out.println(average);
+				
+				// 10. Write an enhanced for loop that prints out each number in the array 
+				//			only if the number is odd
+				for (int element : array) {
+					if (element % 2==1) {
+						System.out.println(element);
+					}
+				}
+				
+				// 11. Create an array that contains the values "Sam", "Sally", "Thomas", and "Robert"
+				String[] names = {"Sam", "Sally", "Thomas", "Robert"};
+				
+				// 12. Calculate the sum of all the letters in the new array
+				int nameSum = 0;
+				for (String name : names) {
+					nameSum += name.length();
+				}
+				
+
+				//
+				// Methods:
+				//
+				
+				// 13. Write and test a method that takes a String name and prints out a greeting. 
+				//			This method returns nothing.
+				greeting("Mario");
+				
+				// 14. Write and test a method that takes a String name and  
+				//			returns a greeting.  Do not print in the method.
+				greeting2("Emily");
+				
+				// Compare method 13 and method 14:  
+				//		a. Analyze the difference between these two methods.
+				//		b. What do you find? 
+				//		c. How are they different?
+				
+				
+				// 15. Write and test a method that takes a String and an int and 
+				//			returns true if the number of letters in the string is greater than the int
+				
+				
+				// 16. Write and test a method that takes an array of string and a string and 
+				//			returns true if the string passed in exists in the array
+				System.out.println();
+				
+				// 17. Write and test a method that takes an array of int and 
+				//			returns the smallest number in the array
+			
+				
+				// 18. Write and test a method that takes an array of double and 
+				//			returns the average
+				
+
+				// 19. Write and test a method that takes an array of Strings and 
+				//			returns an array of int where each element
+				//			matches the length of the string at that position
+
+						
+				// 20. Write and test a method that takes an array of strings and 
+				//			returns true if the sum of letters for all strings with an even amount of letters
+				//			is greater than the sum of those with an odd amount of letters.
+
+			
+				// 21. Write and test a method that takes a string and 
+				//			returns true if the string is a palindrome
+
+				
+				
+			}
+			
+			// Method 13:
+	public static void greeting(String name) {
+		System.out.println("Hello "+ name);
+	}
+
+			// Method 14:
+	public static String greeting2(String name) {
+		return "Hello, " + name; 
+	}
+			
+			// Method 15:
+	public static boolean isStringLongerThanNumber(String string, int number) {
+		if (string.length() > number) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+			
+			// Method 16:
+	public static boolean doesArrayContainString(String[] array, String string) {
+		for (String str : array){
+			if (str.equals(string)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+			
+			// Method 17:
+	public static int smallestNumber(int[] numbers) {
+		int smallest = numbers[0];
+		for (int number : numbers) {
+			if (number < smallest) {
+				smallest = number;
+			}
+		}
+			return smallest; 
+	}
+			
+			// Method 18:
+	public static double calculateAverage(double[] array) {
+		double sum = 0;
+		for (double number : array) {
+			sum += number;
+		}
+		return sum / array.length;
+	}
+
+			
+			// Method 19:
+	public static int[] extractStringLengths(String[] strings) {
+		int[] results = new int[strings.length];
+		for (int i = 0; i < strings.length; i++) {
+			results[i] = strings[i].length();
+		}
+		return results;
+	}
+			
+			// Method 20:
+	public static boolean hasMoreEvenWordCharacters(String[] array) {
+		int evenLetters = 0;
+		int oddLetters = 0;
+
+		for (String string : array) {
+			if (string.length() % 2 == 0) {
+				evenLetters += string.length();
+			} else {
+				oddLetters += string.length();
+			}
+		}
+
+		return evenLetters > oddLetters;
+	}
+
+			
+			// Method 21:
+	public static boolean isPalindrome(String string) {
+		for (int i = 0; i < string.length() / 2; i++) {
+			if (string.charAt(i) != string.charAt(string.length() - i - 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	}
---
+++ src/Week3SourceControlArraysAndMethods/Week3SourceControlArraysAndMethods.java
@@ -0,0 +1,20 @@
+package Week3SourceControlArraysAndMethods;
+
+public class Week3SourceControlArraysAndMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String fullName = "Mario Morales";
+		int[] grades = {100, 90, 80, 92, 100};
+		
+		System.out.println("Student: " + fullName);
+		System.out.println("Grades");
+		for (int i = 0; i < 5; i++) {
+			System.out.println((i+1) + ": " + grades[i]);
+			}
+
+
+	}
+
+}
